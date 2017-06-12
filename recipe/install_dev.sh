9d7f1e5151cce6034289965d5542745c13bd7c64
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-06-12 14:26:13 -0600

    Upload multiple output packages

--- recipe/install_dev.sh
+++ recipe/install_dev.sh
@@ -1,2 +1,4 @@
+echo -n "Installing -dev files..."
 cp -a $SRC_DIR/install/$PREFIX/include/* $PREFIX/include
 cp -a $SRC_DIR/install/$PREFIX/lib/*.{a,la} $PREFIX/lib
+echo " done"
--- recipe/install_main.sh
+++ recipe/install_main.sh
@@ -1,2 +1,4 @@
+echo -n "Installing main files..."
 rm -rf $SRC_DIR/install/$PREFIX/lib/*.{a,la}
 cp -av $SRC_DIR/install/$PREFIX/{bin,lib} $PREFIX
+echo "done"
